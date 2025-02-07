c3e321de40a5993f79dce5bf2a465de943794e1b
João Oliveira <44142506+Joaopmoliveira@users.noreply.github.com>
2025-02-07 10:16:23 +0000

    Update x64-windows-mixed.cmake

--- triplets/x64-windows-mixed.cmake
+++ triplets/x64-windows-mixed.cmake
@@ -1,9 +1,4 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE static)
+set(VCPKG_LIBRARY_LINKAGE static)
 
-if(${PORT} MATCHES "rbdl|vsg|vsgimgui|vsgxchange")
-	set(VCPKG_CRT_LINKAGE static)
-	set(VCPKG_LIBRARY_LINKAGE static)
-else()
-	set(VCPKG_CRT_LINKAGE dynamic)
-	set(VCPKG_LIBRARY_LINKAGE dynamic)
-endif()
